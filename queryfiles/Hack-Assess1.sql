719abab3c00883f72766880f8dce45a8a6eecb38
Matt Deakyne <m.d@ku.edu>
2020-07-14 13:40:02 +0000

    Added Hackboard content

---
+++ queryfiles/Hack-Assess1.sql
@@ -0,0 +1,35 @@
+select
+    year(lt.start_date) as term_year,
+    ifnull(lt.name, 'Unknown Term') as term,
+    replace(ifnull(h2.name, 'No Parent'),'NoName', 'Institution') as hierarchy_parent_node,
+    ifnull(h1.name, 'No Node') as hierarchy_node,
+    count(distinct lc.id) as course_count,
+    count(distinct lp.id) as student_count,
+    student_count/course_count as students_per_course
+from cdm_lms.course lc
+inner join cdm_lms.institution_hierarchy_course ihc
+    on lc.id = ihc.course_id
+    and ihc.primary_ind = 1
+    and ihc.row_deleted_time is null
+left join cdm_lms.institution_hierarchy h1
+    on ihc.institution_hierarchy_id = h1.id
+left join cdm_lms.institution_hierarchy h2
+    on h1.institution_hierarchy_parent_id = h2.id 
+left join cdm_lms.term lt
+    on lt.id = lc.term_id
+left join cdm_lms.person_course pc
+    on lc.id = pc.course_id
+left join cdm_lms.person lp
+    on pc.person_id = lp.id
+where pc.course_role = 'S' and
+    term_year is not null
+group by
+    year(lt.start_date),
+    h1.name,
+    h2.name,
+    lt.name
+order by
+    year(lt.start_date) desc,
+    students_per_course desc,
+    lt.name,
+    h1.name
